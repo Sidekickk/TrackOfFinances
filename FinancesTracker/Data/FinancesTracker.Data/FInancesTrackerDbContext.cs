b8df6750b339d67648b5b94d322f343888666791
Sidekickk <sidekick19323@gmail.com>
2017-09-20 12:26:53 +0300

    Fixed bug with packages

---
+++ FinancesTracker/Data/FinancesTracker.Data/FInancesTrackerDbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace FinancesTracker.Data
+{
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using FinancesTracker.Data.Models;
+
+    public class FinancesTrackerDbContext : IdentityDbContext<User>
+    {
+        public FinancesTrackerDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static FinancesTrackerDbContext Create()
+        {
+            return new FinancesTrackerDbContext();
+        }
+    }
+}
---
+++ FinancesTracker/Web/FinancesTracker.Web/Models/BindingModels/RegisterExternalBindingModel.cs
@@ -0,0 +1,11 @@
+﻿namespace FinancesTracker.Web.Models.BindingModels
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class RegisterExternalBindingModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
